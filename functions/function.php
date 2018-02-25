727b7936f4f3c23bebf18708daad3206e22e0ef4
Rishav Ranjan <rishavbits2016@gmail.com>
2018-02-25 18:41:29 +0530

    Add files via upload

---
+++ functions/function.php
@@ -0,0 +1,5 @@
+<?php
+function getpart($part)
+{
+  require_once 'includes/'."$part".'.php';
+}
---
+++ includes/banner.php
@@ -0,0 +1,4 @@
+<div class="main-banner banner text-center">
+	  <div class="container">
+	  </div>
+	</div>
